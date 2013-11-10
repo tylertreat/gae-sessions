e9eb102547927a0a4dc881b1c2857f0caff2a09b
Tyler Treat <ttreat31@gmail.com>
2013-11-09 20:28:27 -0600

    Add setup.py
    Make this sucker pip installable.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+
+setup_args = dict(
+    name='gae-sessions',
+    version='1.0.7',
+    license='Apache',
+    description='Fast, lightweight Sessions middleware for Google App Engine '
+                '(secure cookies, memcache, or datastore)',
+    author='David Underhill',
+    author_email='dgu@cs.stanford.edu',
+    url='https://github.com/dound/gae-sessions',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: Apache',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
+
+
+if __name__ == '__main__':
+    setup(**setup_args)
